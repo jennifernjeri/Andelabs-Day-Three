b7b5496be807a4878c5c0474d5f00717f1edf58c
jennifernjeri <jenniferliz030@gmail.com>
2017-01-19 11:59:31 +0300

    Add files via upload

---
+++ missingnumbers.py
@@ -0,0 +1,11 @@
+def missing_number(list1,list2):
+
+  if list1 == [] and list2 == []:
+    return 0
+
+  if list1 == [ ] and list2 == [ ]:
+      return 0
+
+  else:
+      if list1 == [] and list2 == [ ]:
+          return missing_number
